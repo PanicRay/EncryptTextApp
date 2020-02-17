72435d6dc07ae73567ddd76bbad1060e0b9544dc
Raydrrrr1234 <ruikang.dai@gmail.com>
2020-02-17 17:38:38 +0800

    Solved: the default password bar is not showing.

--- app/src/main/java/dk/meznik/jan/encrypttext/MainActivity.java
+++ app/src/main/java/dk/meznik/jan/encrypttext/MainActivity.java
@@ -3,13 +3,11 @@ package dk.meznik.jan.encrypttext;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.Switch;
 
 public class MainActivity extends AppCompatActivity {
 
     public static String version = "ver1.0"; // Warning, different cannot talk to each other!
-    public static int selection = 0;
-    private Switch useDefault = (Switch)findViewById(R.id.switch2);
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
